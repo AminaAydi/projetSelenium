c743b5ed32341493d2359a832af60bc742d84273
user <user@DESKTOP-F7MMGLQ>
2025-05-25 01:24:25 +0200

    version 2

--- projetObjectModel/src/test/java/run/Runner.java
+++ projetObjectModel/src/test/java/run/Runner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		tags = "@Register" ,
-		glue = {"stepDefinition","hooks"},
+		glue = {"stepDefinition", "hooks"},
 		features = "src/test/features/testCasesAutomationExercices" ,
 		publish = true ,
 		plugin = {"pretty","html:target/cucumber-report.html","json:target/cucumber-reportJson","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
